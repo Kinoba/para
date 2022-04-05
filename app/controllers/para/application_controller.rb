d125d6b3cdf76d49548649e014cdbaf650194422
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2022-04-05 14:30:50 +0200

    fix(rails): add switch_locale method for Rails >= 5.0

--- app/controllers/para/application_controller.rb
+++ app/controllers/para/application_controller.rb
@@ -1,5 +1,7 @@
 module Para
   class ApplicationController < ActionController::Base
+    prepend_around_action :switch_locale
+
     before_action :add_admin_home_breadcrumb
 
     def admin?
@@ -10,6 +12,14 @@ module Para
 
     def add_admin_home_breadcrumb
       add_breadcrumb :home, admin_path
+    end
+
+
+    private
+
+    def switch_locale(&action)
+      current_locale = params[:locale].presence || I18n.default_locale
+      I18n.with_locale(current_locale.to_sym, &action)
     end
   end
 end
