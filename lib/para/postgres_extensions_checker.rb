547f8e2120280ebee2c949b670c50efc5824e5f2
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2024-12-26 16:42:05 +0100

    chore: add NoMethodError to rescued in postgres extension checker

--- lib/para/postgres_extensions_checker.rb
+++ lib/para/postgres_extensions_checker.rb
@@ -2,16 +2,16 @@ module Para
   class PostgresExtensionsChecker
     class << self
       def check_all
-        %w(hstore unaccent).each do |extname|
-          unless extension_exists?(extname)
-            # Could not use Rails.logger here, using puts as a temporary
-            # solution.
-            puts "[Warning] PostgreSQL \"#{ extname }\" extension is not " +
-                 "installed in your database. This means that you " +
-                 "missing some migrations that you can install " +
-                 "with the following command : " +
-                 "`rake para_engine:install:migrations` and then migrate."
-          end
+        %w[hstore unaccent].each do |extname|
+          next if extension_exists?(extname)
+
+          # Could not use Rails.logger here, using puts as a temporary
+          # solution.
+          puts "[Warning] PostgreSQL \"#{extname}\" extension is not " +
+               'installed in your database. This means that you ' +
+               'missing some migrations that you can install ' +
+               'with the following command : ' +
+               '`rake para_engine:install:migrations` and then migrate.'
         end
       end
 
@@ -19,10 +19,10 @@ module Para
 
       def extension_exists?(extname)
         ActiveRecord::Base.connection.execute(
-          "SELECT COUNT(*) FROM pg_catalog.pg_extension " +
-          "WHERE extname = '#{ extname }'"
+          'SELECT COUNT(*) FROM pg_catalog.pg_extension ' +
+          "WHERE extname = '#{extname}'"
         ).first['count'].to_i > 0
-      rescue ActiveRecord::NoDatabaseError
+      rescue ActiveRecord::NoDatabaseError, NoMethodError
         true # Do not issue warning when no database is installed
       end
     end
